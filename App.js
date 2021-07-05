4513ed8253cedeaa1d09f57bef6a472e6bcad4ee
lofiandcode <lofiandcode@gmail.com>
2021-07-05 15:01:00 -0700

    Add an array for verbs

--- App.js
+++ App.js
@@ -1,2 +1,4 @@
 const positiveAdjectives1 = ['adaptable', 'affectionate', 'amiable', 'brave', 'considerate', 'courteous', 'empathetic', 'frank', 'good', 'impartial', 'inventive', 'persistent', 'practical', 'reliable', 'sensible', 'sincere', 'sympathetic', 'witty'];
 const positiveAdjectives2 = ['adventurous', 'ambitious', 'chatty', 'compassionate', 'courageous', 'diligent', 'exuberant', 'funny', 'generous', 'gregarious', 'intuitive', 'passionate', 'philosophical', 'rational', 'resourceful', 'smart', 'unassuming'];
+const verbs = ['admire', 'are just "meh" when it comes to', 'could take or leave', 'idolize', 'like'];
+
