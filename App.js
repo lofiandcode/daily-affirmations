93c67f95df18c84708b24accf1e6523e61844f9c
lofiandcode <lofiandcode@gmail.com>
2021-07-05 15:28:03 -0700

    Add function that prints montra to the console

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 const positiveAdjectives1 = ['adaptable', 'affectionate', 'amiable', 'brave', 'considerate', 'courteous', 'empathetic', 'frank', 'good', 'impartial', 'inventive', 'persistent', 'practical', 'reliable', 'sensible', 'sincere', 'sympathetic', 'witty'];
 const positiveAdjectives2 = ['adventurous', 'ambitious', 'chatty', 'compassionate', 'courageous', 'diligent', 'exuberant', 'funny', 'generous', 'gregarious', 'intuitive', 'passionate', 'philosophical', 'rational', 'resourceful', 'smart', 'unassuming'];
-const verbs = ['admire', 'are just "meh" when it comes to', 'could take or leave', 'idolize', 'like'];
+const verbs = ['admire', 'idolize', 'like', 'worship'];
 
 const randIndex = arrLength => {
     return Math.floor(Math.random() * arrLength);
 }
 
+const Montra = () => {
+    const adjective1 = positiveAdjectives1[randIndex(positiveAdjectives1.length)];
+    const adjective2 = positiveAdjectives2[randIndex(positiveAdjectives2.length)];
+    const verb = verbs[randIndex(verbs.length)];
+
+    console.log(`I'm ${adjective1} enough,`);
+    console.log(`I'm ${adjective2} enough,`);
+    console.log(`And gosh darn it, people ${verb} me!`);
+}
+
+const main = () => {
+    
+    Montra();
+}
+
+main()
